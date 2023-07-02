5ac2c13eaa0d98b804b7fc3b763f93d1f3193acb
Eunbi An <ibnuena@naver.com>
2023-07-03 00:21:58 +0900

    귤

--- eunbi/week1/귤고르기.java
+++ eunbi/week1/귤고르기.java
@@ -1,5 +1,33 @@
 package eunbi.week1;
 
-public class 귤고르기 {
 
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        
+        List<Integer> t = Arrays.stream(tangerine)
+                        .boxed()
+                        .collect(Collectors.toList()); //list로 변환      
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < tangerine.length; i++)
+            map.put(tangerine[i], map.getOrDefault(tangerine[i], 0) + 1);
+        
+        
+        List<Integer> list = new ArrayList<>(map.values());
+        
+        list.sort(Comparator.reverseOrder());        
+            
+        int i = 0;
+        while(k>0){
+            k = k - list.get(i);
+            answer++;
+            i++;
+        }
+        return answer;
+    }
 }
