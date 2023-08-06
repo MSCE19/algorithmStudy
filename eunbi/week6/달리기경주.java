606b6019b328c8fc313f2e019018c1a97e1537c9
Eunbi An <ibnuena@naver.com>
2023-08-06 20:37:03 +0900

    달리기경주

--- eunbi/week6/달리기경주.java
+++ eunbi/week6/달리기경주.java
@@ -1,5 +1,25 @@
 package algorithmStudy;
 
-public class 달리기경주 {
+import java.util.*;
 
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+        String[] answer = new String[players.length];
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        
+        for(int i=0; i<players.length; i++)
+            map.put(players[i], i);
+        
+        for(int i=0; i<callings.length; i++){
+            int index = map.get(callings[i]);
+            
+            players[index] = players[index-1];
+            players[index-1] = callings[i];
+            
+            map.put(players[index], index);
+            map.put(players[index-1], index-1);
+        } 
+        
+        return players;
+    }
 }
