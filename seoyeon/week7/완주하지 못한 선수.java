f349b05c35e0dad21be8118c59bea95a23a02378
seoyeon08 <silversun0808@gmail.com>
2023-08-09 22:52:07 +0900

    [Add] 완주하지 못한 선수.java

---
+++ seoyeon/week7/완주하지 못한 선수.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        int i = 0;
+        //언제나 completion이 한 명 짧으니까
+        for(i = 0; i < completion.length; i++){
+            // 없는 사람을 찾아서 바로 return
+            if(!participant[i].equals(completion[i])){
+                return participant[i];
+            }
+        }
+        return participant[i];
+    }
+}
