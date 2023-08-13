5a3c61b1fb4398cbf506a74ecc8a8c58d564978e
seoyeon08 <silversun0808@gmail.com>
2023-08-13 17:27:15 +0900

    [Add] 대충 만든 자판.py

---
+++ seoyeon/week7/대충 만든 자판.py
@@ -0,0 +1,18 @@
+def solution(keymap, targets):
+    answer = []
+    keytable = {}
+    for i in keymap :
+        for idx, j in enumerate(i) :
+            if j in keytable :
+                keytable[j] = min(keytable[j], idx+1)
+            else :
+                keytable[j] = idx+1
+    for i in targets :
+        tmp = 0
+        for j in i :
+            if j not in keytable:
+                tmp = -1
+                break
+            tmp += keytable[j]
+        answer.append(tmp)
+    return answer
